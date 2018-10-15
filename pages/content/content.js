a0847797952665d0745e48f73dff80b323044568
yeykai <707443648@qq.com>
2018-10-15 13:33:50 +0800

    商品详情页与留言功能完成
    商品详情页图片轮播
    收藏商品、分享商品（未做）
    用户可给商品留言
    下一步：商品订单功能

--- pages/content/content.js
+++ pages/content/content.js
@@ -13,13 +13,25 @@ Page({
     duration: 1000,
     imageURL: "/images/44.png",
 
+    likeIcon:"/img/like.png",
+
+    islikeGoods:false,
+    
+    goodsInfo:{},
+
     goodsName:"",
     goodsDesc:"",
     address:"",
     likeCounts:"",
     price:"",
     nickName: "",
-    avatar_url: ""
+    avatar_url: "",
+
+
+    commentsPage: 1,
+    commentsTotalPage: 1,
+    commentsList: [],
+    placeholder: "说点什么..."
   },
 
   /**
@@ -27,6 +39,7 @@ Page({
    */
   onLoad: function (params) {
     var me = this;
+    var thirdSession = wx.getStorageSync("thirdsession")
 
     // 获取上一个页面传入的参数
     var goodsInfo = JSON.parse(params.goodsInfo);
@@ -37,8 +50,13 @@ Page({
     var price = goodsInfo.price;
     var nickName = goodsInfo.nickName;
     var avatar_url = goodsInfo.avatar_url;
+    var goodsId = goodsInfo.id;
+    var sellerId = goodsInfo.sellerId;
 
     me.setData({
+      goodsInfo: goodsInfo,
+      goodsId: goodsId,
+      sellerId: sellerId,
       goodsName: goodsName,
       goodsDesc: goodsDesc,
       address: address,
@@ -56,11 +74,24 @@ Page({
       success: function (res) {
         console.log(res);
         me.setData({
-          imgUrls : res.data.data,
+          imgUrls: res.data.data,
           serverUrl: serverUrl,
         })
       }
     })
+
+    wx.request({
+      url: serverUrl + '/goods/queryUserLikeGoods?thirdSession=' + thirdSession + "&goodsId=" + goodsInfo.id,
+      method: 'POST',
+      success: function (res) {
+        console.log(res);
+        me.setData({
+          islikeGoods: res.data.data
+        })
+      }
+    })
+
+    me.getCommentsList(1);
   },
 
   previewImg: function (e) {
@@ -91,6 +122,159 @@ Page({
     })
   },
 
+
+  likeGoods:function(e){
+    var me = this;
+
+    var goodsId = me.data.goodsId;
+    var sellerId = me.data.sellerId;
+    var likeIcon = me.data.likeIcon;
+    var serverUrl = app.serverUrl;
+    var thirdSession = wx.getStorageSync("thirdsession")
+
+    var url = '/goods/userLike?thirdSession=' + thirdSession + '&goodsId=' + goodsId + '&sellerId=' + sellerId;
+    
+    if (thirdSession == null || thirdSession == undefined || thirdSession == '') {
+      wx.navigateTo({
+        url: '../wxLogin/login',
+      })
+    } else{
+       
+        var islikeGoods = me.data.islikeGoods
+        var likeCounts = me.data.likeCounts
+
+        if (islikeGoods) {
+          me.setData({
+            likeCounts: likeCounts-1
+          });
+          url = '/goods/userUnLike?thirdSession=' + thirdSession + '&goodsId=' + goodsId + '&sellerId=' + sellerId;
+        } else{
+          me.setData({
+            likeCounts: likeCounts + 1
+          });
+        }
+     
+      wx.showLoading({
+        title: '...',
+      })
+
+      wx.request({
+        url: serverUrl + url,
+        method: 'POST',
+        header: {
+          'content-type': 'application/json', // 默认值
+          // 'userId': user.id,
+          // 'userToken': user.userToken
+        },
+        success: function (res) {
+          wx.hideLoading();
+          me.setData({
+            islikeGoods: !islikeGoods,
+          });
+        }
+      })
+
+    }
+    
+  },
+
+
+
+  saveComment: function (e) {
+    var me = this;
+    var content = e.detail.value.comments;
+    console.log(content);
+    // 获取评论回复的fatherCommentId和toUserId
+    // var fatherCommentId = e.currentTarget.dataset.replyfathercommentid;
+    // var toUserId = e.currentTarget.dataset.replytouserid;
+    var fatherCommentId =  me.data.replyFatherCommentId;
+    var toUserId = me.data.replyToUserId;
+    console.log(fatherCommentId);
+    console.log(toUserId);
+    var thirdSession = wx.getStorageSync("thirdsession");
+    var videoInfo = JSON.stringify(me.data.videoInfo);
+
+
+      wx.showLoading({
+        title: '请稍后...',
+      })
+      wx.request({
+        url: app.serverUrl + '/goods/saveComment?fatherCommentId=' + fatherCommentId + "&toUserId=" + toUserId+"&thirdSession=" + thirdSession,
+        method: 'POST',
+        header: {
+          'content-type': 'application/json', // 默认值
+        },
+        data: {
+          goodsId: me.data.goodsInfo.id,
+          comment: content
+        },
+        success: function (res) {
+          console.log(res.data)
+          wx.hideLoading();
+
+          me.setData({
+            contentValue: "",
+            commentsList: [],
+            placeholder: "",
+            replyFatherCommentId: "",
+            replyToUserId: "",
+            commentFocus: false
+          });
+
+
+          me.getCommentsList(1);
+        }
+      })
+
+  },
+
+
+  //分享商品
+  shareGoods:function(){
+
+  },
+
+
+
+  //获取评论列表
+  getCommentsList: function (page) {
+    var me = this;
+
+    var goodsId = me.data.goodsInfo.id;
+
+    wx.request({
+      url: app.serverUrl + '/goods/getGoodsComments?goodsId=' + goodsId + "&page=" + page + "&pageSize=5",
+      method: "POST",
+      success: function (res) {
+        console.log(res.data);
+
+        var commentsList = res.data.data.rows;
+        var newCommentsList = me.data.commentsList;
+
+        me.setData({
+          commentsList: newCommentsList.concat(commentsList),
+          commentsPage: page,
+          commentsTotalPage: res.data.data.total
+        });
+      }
+    })
+  },
+
+  reply:function(e){
+    var me = this
+    console.log(e);
+    var fatherCommentId = e.currentTarget.dataset.fathercommentid;
+    var toUserId = e.currentTarget.dataset.touserid;
+    var toNickname = e.currentTarget.dataset.tonickname;
+
+    me.setData({
+      placeholder: "回复  " + toNickname,
+      replyFatherCommentId: fatherCommentId,
+      replyToUserId: toUserId,
+      commentFocus: true
+    });
+
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
